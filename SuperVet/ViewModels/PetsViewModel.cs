6c58358eef842d3674f76683b4ff632e92e548c6
Aydan Murshud <aydan.murshud@accedia.com>
2023-10-03 16:37:31 +0300

    feat: CRUD Operations in all levels

---
+++ SuperVet/ViewModels/PetsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SuperVet.Models;
+
+namespace SuperVet.ViewModels
+{
+    public class PetsViewModel
+    {
+        public int Id { get; set; }
+
+        public string AnimalType { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public List<Species>? Species { get; set; }
+    }
+}
---
+++ SuperVet/ViewModels/SpeciesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SuperVet.Models;
+
+namespace SuperVet.ViewModels
+{
+    public class SpeciesViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Image { get; set; }
+        public List<Breeds>? Breeds { get; set; }
+        public int PetsId { get; set; }
+        public List<Pets>? Pets { get; set; }
+    }
+}
